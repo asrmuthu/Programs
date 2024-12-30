cac461759724cf14e3b8e67074a9595450cdcc60
asrmuthu <asrmuthu57@example.com>
2024-12-30 10:38:58 +0530

    router

--- Programs/React/browserrouter/src/App.js
+++ Programs/React/browserrouter/src/App.js
@@ -1,11 +1,22 @@
+import {Routes, Route } from 'react-router-dom'; 
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import Header from './Header';
+import Nav from './Nav';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Browser Router</h1>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
   );
 }
 
