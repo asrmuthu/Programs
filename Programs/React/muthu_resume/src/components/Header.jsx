dcf9cc3d202339c93b57b4889c0466eb22002b9f
asrmuthu <asrmuthu57@example.com>
2024-12-31 01:49:38 +0530

    Header

---
+++ Programs/React/muthu_resume/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import muthu_image from "../muthu_image.jpg";
+import "../css/Header.css";
+import { GrLocation } from "react-icons/gr";
+import { FaMobileRetro } from "react-icons/fa6";
+import { AiFillMail } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
+
+const Header = () => {
+return (
+    <div>
+        <header className="App-header">
+            <hgroup className="hgroup">
+                <img src={muthu_image} className="muthu_image" alt="logo" />
+                <h1>Muthupandi Raj</h1>
+                <h2>Engineering Lead</h2>
+                <div className="social">
+                    <a href="https://www.linkedin.com/feed/"><AiFillLinkedin /></a>
+                    <a href="https://github.com/asrmuthu/Programs/tree/main/Programs"><AiFillGithub /></a>
+                </div>
+                <p className="context">CONTACT:</p>
+                <p className="contact-info"><GrLocation /> Theni, Tamil Nadu, India.</p>
+                <p className="contact-info"><FaMobileRetro /> 7904688429</p>
+                <p className="contact-info"><AiFillMail /> asrmuthu57@gmail.com</p>
+            </hgroup>
+        </header>
+    </div>
+);
+};
+
+export default Header;
