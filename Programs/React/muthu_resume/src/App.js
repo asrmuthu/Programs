e9e3256fecec1c0f2d8782a809fb4dad29b4227d
asrmuthu <asrmuthu57@example.com>
2024-12-31 14:04:22 +0530

    resume

--- Programs/React/muthu_resume/src/App.js
+++ Programs/React/muthu_resume/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Details from "./components/Details";
 import Header from "./components/Header";
+import useWindowSize from './components/useWindowSize';
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
 
 function App() {
+  const { width } = useWindowSize();
   return (
     <div className="App">
-      <div className="App-container">
+       {width < 768 ? <FaMobileAlt />
+                : width < 992 ? <FaTabletAlt />
+                    : <FaLaptop />}
+      <div className="App-container" style={{ display: width < 900 ? 'block' : 'flex' }}>
         <Header />
         <Details />
       </div>
