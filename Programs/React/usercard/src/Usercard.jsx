e96968e98e8b2b82aa982716861c2fb8b47d9e81
asrmuthu <asrmuthu57@example.com>
2025-01-01 19:36:11 +0530

    com

--- Programs/React/usercard/src/Usercard.jsx
+++ Programs/React/usercard/src/Usercard.jsx
@@ -1,46 +1,48 @@
-import React from 'react'
-import User from './User'
+import React from 'react';
+import User from './User';
 
+const profiles = [
+  {
+    name: "Muthupandi Raj",
+    city: "Theni",
+    role: "Lead Developer",
+    skills: ["React", "Javascript", "css", "HTML", "Node", "XSLT", "C# .NET", "SQL"],
+    online: true,
+    image: "images/1.jpg",
+  },
+  {
+    name: "Mihran",
+    city: "Coimbatore",
+    role: "Manager",
+    skills: ["HTML", "Javascript", "css", "React", "Node", "JAVA", "C# .NET", "SQL"],
+    online: false,
+    image: "images/2.jpg",
+  },
+  {
+    name: "Renuka",
+    city: "Chennai",
+    role: "Developer",
+    skills: ["React", "Javascript", "css", "HTML", "Node", "XSLT", "C# .NET", "SQL"],
+    online: true,
+    image: "images/3.jpg",
+  },
+];
 
-const Usercard = () => {
-  const profiles = [
-    { name : "Muthupandi Raj",
-      city : "Theni",
-      role : "Lead Developer",
-      skills: ["React","Javascript","css","HTML","Node","XSLT","C# .NET","SQL"],
-      online: true,
-      image:"images/1.jpg",
-    },
-    {
-      name : "Mihran",
-      city : "Coimbatore",
-      role : "Manager",
-      skills: ["HTML","Javascript","css","React","Node","JAVA","C# .NET","SQL"],
-      online: false,
-      image:"images/2.jpg",
-    },
-    {
-      name : "Renuka",
-      city : "Chennai",
-      role : "Developer",
-      skills: ["React","Javascript","css","HTML","Node","XSLT","C# .NET","SQL"],
-      online: true,
-      image:"images/3.jpg",
-    },
-  ]
-  return (
-    <>
-    {profiles.map((profile, index) => (<User key={index} 
-    name={profile.name} 
-    city={profile.city}
-    role={profile.role}
-    skills={profile.skills}
-    online={profile.online}
-    image={profile.image}
-    />))}
-    </>
-    // <User name="Muthupandi Raj" city ="Theni" role ="Lead Developer" skills = {["React","Javascript","css","HTML","Node","XSLT","C# .NET","SQL"]} online = {true} image = "images/1.jpg"/>
-  )
-}
+const Usercard = () => (
+  <>
+    {profiles.map((profile, index) => (
+      <User key={index} {...profile} />
+    ))}
+  </>
+);
+// {profiles.map((profile, index) => (<User key={index} 
+//   name={profile.name} 
+//   city={profile.city}
+//   role={profile.role}
+//   skills={profile.skills}
+//   online={profile.online}
+//   image={profile.image}
 
-export default Usercard
+//   <User name="Muthupandi Raj" city ="Theni" role ="Lead Developer" skills = {["React","Javascript","css","HTML","Node","XSLT","C# .NET","SQL"]} online = {true} image = "images/1.jpg"/>
+
+export default Usercard;
