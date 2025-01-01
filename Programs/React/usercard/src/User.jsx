422b945792f526b2a3889baffa3aaa3bc34b1e2c
asrmuthu <asrmuthu57@example.com>
2025-01-01 13:37:36 +0530

    usercard full code

---
+++ Programs/React/usercard/src/User.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const User = ({name, city, role, skills, online, image}) => {
+  return (
+    <div className="card-container">
+      <span className={online ? "pro online" : "pro offline"}>{online ? "ONLINE" : "OFFLINE"}</span>
+      <img src={image} alt="user" />
+      <h3>{name}</h3>
+      <h3>{city}</h3>
+      <p>{role}</p>
+      <div className="buttons">
+        <button>Message</button>
+        <button className="outline">Following</button>
+      </div>
+      <div className="skills">
+        <h6>Skills:</h6>
+        <ul>
+          {skills.map((skill,index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default User;
