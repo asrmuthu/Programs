c13dbedad6f7c765968043d6489543b1d0f9e32e
asrmuthu <asrmuthu57@example.com>
2025-01-03 17:01:09 +0530

    no error

--- Programs/React/currency_converter/src/App.js
+++ Programs/React/currency_converter/src/App.js
@@ -1,8 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { MdCurrencyExchange } from "react-icons/md";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromcurrency, setFromCurrency] = useState("USD");
+  const [tocurrency, setToCurrency] = useState("INR");
+  const [exchangeRate, setExchangeRate] = useState(82); // 1 USD = 82 INR
+useEffect(() => {
+  const getExchangeRate = async () => {
+    try{
+      let url =`https://api.exchangerate-api.com/v4/latest/${fromcurrency}`
+      const response = await axios.get(url);
+      console.log(response.data.rates);
+      console.log("response",response);
+      
+    }
+    catch(error){
+      console.log("Error in fetching exchange rate", error);
+    }
+  }
+})
+
+const handleAmount = (e) => {
+  const val = parseFloat(e.target.value);
+  setAmount(isNaN(val) ?  0 : val);
+}
+
   return (
     <div className="App">
       <div className="logo">
@@ -14,21 +40,21 @@ function App() {
         <div className="input">
           <div className="amount">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" className="textbox"/>
+            <input type="number" id="amount" className="textbox" value={amount} onChange={handleAmount}/>
           </div>
           <div className="from">
             <label htmlFor="from">From Currency</label>
-            <select id="from">
-              <option value="inr">INR</option>
+            <select id="from" value={fromcurrency}>
+              <option value="USD">{fromcurrency}</option>
             </select>
           </div>
-          <div className="to">
+          <div className="to" value={tocurrency}>
             <label htmlFor="to">To Currency</label>
             <select id="to">
-              <option value="inr">INR</option>
+              <option value="INR">{tocurrency}</option>
             </select>
           </div>
-          <p>I USD is Equal to 82 INR</p>
+          <p>{amount} {fromcurrency} is Equal to {exchangeRate} {tocurrency}</p>
         </div>
       </div>
     </div>
