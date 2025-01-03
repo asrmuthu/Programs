a3c6dad1f68421206b5af54a2f790afed095354a
asrmuthu <asrmuthu57@example.com>
2025-01-03 16:18:38 +0530

    css complete

--- Programs/React/currency_converter/src/App.js
+++ Programs/React/currency_converter/src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MdCurrencyExchange } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="logo">
+        <MdCurrencyExchange />
+      </div>
+      
+      <div className="data">
+        <h2>Currency Converter</h2>
+        <div className="input">
+          <div className="amount">
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount" className="textbox"/>
+          </div>
+          <div className="from">
+            <label htmlFor="from">From Currency</label>
+            <select id="from">
+              <option value="inr">INR</option>
+            </select>
+          </div>
+          <div className="to">
+            <label htmlFor="to">To Currency</label>
+            <select id="to">
+              <option value="inr">INR</option>
+            </select>
+          </div>
+          <p>I USD is Equal to 82 INR</p>
+        </div>
+      </div>
     </div>
   );
 }
