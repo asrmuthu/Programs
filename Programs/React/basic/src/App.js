49a538709395fe4be2d3397c9e1962c7014cc22b
asrmuthu <asrmuthu57@example.com>
2025-03-07 02:40:14 +0530

    aaa

--- Programs/React/basic/src/App.js
+++ Programs/React/basic/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Alert from './components/Alert';
+import GetAPI_axios from './components/GetAPI_axios';
+import GetAPI_Axios_complex from './components/GetAPI_Axios_complex';
+import PostAPi_axios from './components/PostAPi_axios';
 import Calculator from './components/Calculator';
 import RandomNameChange from './components/RandomNameChange';
 import Date from './components/Date';
@@ -12,12 +15,16 @@ import SimpleUseState from './components/SimpleUseState';
 import SimpleUseState2 from './components/SimpleUseState2';
 import MediumUseState from './components/MediumUseState';
 import Use_effact from './components/Use_effact';
+import { Axios } from 'axios';
 
 function App() {
   return (
     <div className="App">
       <Alert />
-      <Calculator />
+      {/* <GetAPI_axios /> */}
+      {/* <GetAPI_Axios_complex /> */}
+      <PostAPi_axios />
+      {/* <Calculator />
       <RandomNameChange />
       <Date />
       <FunctionCalling />
@@ -27,7 +34,7 @@ function App() {
       <SimpleUseState />
       <SimpleUseState2 />
       <MediumUseState />
-      <Use_effact />
+      <Use_effact /> */}
     </div>
   );
 }
