aaf6b7906a67541508ff9b5da5e436f0737dd075
asrmuthu <asrmuthu57@example.com>
2025-03-10 14:43:58 +0530

    a

--- coworking-booking/src/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import centersReducer from "./features/centersSlice";
-
-export const store = configureStore({
-  reducer: { centers: centersReducer },
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
