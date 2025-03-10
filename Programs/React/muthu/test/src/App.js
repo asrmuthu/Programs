bd9c801570598e7a04fb19f36fc287d84152b9de
asrmuthu <asrmuthu57@example.com>
2025-03-10 23:33:32 +0530

    aaa

--- Programs/React/muthu/test/src/App.js
+++ Programs/React/muthu/test/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Increment from './Components/Increment';
+import Hide_Show from './Components/Hide_Show';
 
 function App() {
   return (
     <div className="App">
-      <Increment />
+      {/* <Increment /> */}
+      <Hide_Show />
     </div>
   );
 }
