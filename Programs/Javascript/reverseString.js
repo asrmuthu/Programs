c6ddddb70f541fadb360818ae3e037b8fe1de5a0
asrmuthu <asrmuthu57@example.com>
2025-03-21 05:53:45 +0530

    ms

---
+++ Programs/Javascript/reverseString.js
@@ -0,0 +1,8 @@
+let s = "Muthu";
+const ans = s.split('').reverse().join('');
+console.log(ans);  // Output: uhtuM
+
+const array1 = ["one", "two", "three"];
+const reversed = array1.reverse();
+console.log("reversed:", reversed);
+// Expected output: "reversed:" Array ["three", "two", "one"]let arr = [1, 2, 3, 0, -5];
