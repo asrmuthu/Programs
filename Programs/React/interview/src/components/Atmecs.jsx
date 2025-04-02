10d7d8a105d36a43c9a433ef867178b7f6e093e4
asrmuthu <asrmuthu57@example.com>
2025-04-02 15:48:36 +0530

    kk

--- Programs/React/interview/src/components/Atmecs.jsx
+++ Programs/React/interview/src/components/Atmecs.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react'
 
 const Atmecs = () => {
-  const data = [1, 2, 3, 5, 6];
-  const [val, setVal] = useState("1");
-
   return (
-    <div>
-      <p>App</p>
-      <select onChange={(e) => setVal(e.target.value)}>
-        {data.map((item, index) => (
-          <option key={index}>{item}</option>
-        ))}
-      </select>
-      <p>{val}</p>
-    </div>
-  );
-};
-export default Atmecs;
+    <div>Atmecs</div>
+  )
+}
+
+export default Atmecs
