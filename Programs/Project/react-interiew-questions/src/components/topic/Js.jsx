423161264a32d8670aedc67161ac4b9a6b239d79
asrmuthu <asrmuthu57@example.com>
2025-04-23 09:45:45 +0530

    css

--- Programs/Project/react-interiew-questions/src/components/topic/Js.jsx
+++ Programs/Project/react-interiew-questions/src/components/topic/Js.jsx
@@ -4,6 +4,37 @@ const Js = () => {
   return (
     <>
       <h4>JavaScript Interview Question and Answers</h4>
+      <div>
+        <div className="table-container js-1">
+          <h6>Difference between Var, Let, and Const?</h6>
+          <table>
+            <thead>
+              <tr>
+                <th>Var</th>
+                <th>Let</th>
+                <th>Const</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Var is function scoped.</td>
+                <td>Let is block scoped.</td>
+                <td>Const is also block scoped.</td>
+              </tr>
+              <tr>
+                <td>Var can be redeclared.</td>
+                <td>Let cannot be redeclared.</td>
+                <td>Const cannot be redeclared.</td>
+              </tr>
+              <tr>
+                <td>Var can be reassigned.</td>
+                <td>Let can be reassigned.</td>
+                <td>Const cannot be reassigned.</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
