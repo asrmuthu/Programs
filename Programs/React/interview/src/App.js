dab73d0019b8454306ca820e362b9f50e360117a
asrmuthu <asrmuthu57@example.com>
2025-06-30 14:44:52 +0530

    mmm

--- Programs/React/interview/src/App.js
+++ Programs/React/interview/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Todo from './Important/Todo.jsx'
 import Navigate from './Important/Navigate.jsx'
+import Filehandling from './components/Filehandling.jsx'
+import Ielectron_xslx_file from './components/Ielectron_xslx_file.jsx'
 
 const App = () => {
   return (
     <div>
-        <Navigate />
+      <Ielectron_xslx_file />
     </div>
   )
 }
