7f6434cbed1b96504fa864a033a213b183e29b61
asrmuthu <asrmuthu57@example.com>
2025-06-30 11:11:05 +0530

    s,ss

--- Programs/React/testing/src/App.js
+++ Programs/React/testing/src/App.js
@@ -1,18 +1,3 @@
-// import React, { useState } from "react";
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <h1>count {count}</h1>
-//       <button onClick={() => setCount(count + 1)}>Add</button>
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { useState, useEffect } from "react";
 
 const App = () => {
@@ -22,7 +7,7 @@ const App = () => {
     const API = async () => {
       const res1 = await fetch("https://dummyjson.com/products");
       const res = await res1.json();
-      setItems(res.products); // ✅ correct
+      setItems(res.products);
     };
 
     API();
--- Programs/React/testing/src/App.test.js
+++ Programs/React/testing/src/App.test.js
@@ -1,13 +1,3 @@
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import App from "./App";
-
-//   test("count", () => {
-//   render(<App />);
-//   expect(screen.getByText(/count 0/i)).toBeInTheDocument(); // initial state
-//   fireEvent.click(screen.getByText("Add")); // click the button
-//   expect(screen.getByText(/count 1/i)).toBeInTheDocument(); // updated state
-// });
-
 import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
