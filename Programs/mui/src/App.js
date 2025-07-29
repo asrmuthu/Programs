49fca8e05fb7ab1c28d449f16afb391eb4150a06
asrmuthu <asrmuthu57@example.com>
2025-07-29 09:38:00 +0530

    mui

---
+++ Programs/mui/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const App = () => {
+  return (
+    <div>
+      <Typography variant="h6" component='h1'>sdnfmhsdvb nsd vjhsdgvbhjbsd vmjbsdycvashjcv snmgcvuas cnmasc hjas vchjasvhjasbvkhjas cvkjhasbc8qwubc kjasbcjaksb caskjcvbquabc asnjcbuyevbkjas</Typography>
+    </div>
+  );
+};
+
+export default App;
