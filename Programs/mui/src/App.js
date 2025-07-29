8c29f2136fba85ff495a4b68a0454111966dad87
asrmuthu <asrmuthu57@example.com>
2025-07-29 09:53:52 +0530

    m

--- Programs/mui/src/App.js
+++ Programs/mui/src/App.js
@@ -4,6 +4,9 @@ import Typography from "@mui/material/Typography";
 const App = () => {
   return (
     <div>
+      <Box>
+        
+      </Box>
       <Typography variant="h6" component='h1'>sdnfmhsdvb nsd vjhsdgvbhjbsd vmjbsdycvashjcv snmgcvuas cnmasc hjas vchjasvhjasbvkhjas cvkjhasbc8qwubc kjasbcjaksb caskjcvbquabc asnjcbuyevbkjas</Typography>
     </div>
   );
